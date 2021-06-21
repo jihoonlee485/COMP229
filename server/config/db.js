e6b149445fe4f9e58b2ce1efc758b74fcf2eb4bd
jihoonlee485 <jlee485@my.centennialcollege.ca>
2021-06-21 00:09:34 -0400

    assignment2

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://jihoonlee:<password>@cluster0.ichzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
